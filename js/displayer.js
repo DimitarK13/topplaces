00c6cf6468c866ec7a35854541f47ffe7e996cbe
Dimitar <50843303+DimitarK13@users.noreply.github.com>
2021-10-17 16:25:56 +0200

    Add files via upload

---
+++ js/displayer.js
@@ -0,0 +1,24 @@
+$("section").hide()
+$(".close").click(function(){
+    $("section").hide(1000);
+})
+
+$(".tokyo").click(function(){
+    $("#tokyo").show(1000);
+})
+
+$(".bahamas").click(function(){
+    $("#bahamas").show(1000);
+})
+
+$(".amsterdam").click(function(){
+    $("#amsterdam").show(1000);
+})
+
+$(".mauritius").click(function(){
+    $("#mauritius").show(1000);
+})
+
+$(".moscow").click(function(){
+    $("#moscow").show(1000);
+})
